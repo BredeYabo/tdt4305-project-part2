e870e165cde91700b7d7fa2e980ca58503cccc94
Brede Yabo Kristensen <bredeyabo@hotmail.com>
2019-03-29 18:14:22 +0100

    Finished project

--- recommend.py
+++ recommend.py
@@ -18,49 +18,37 @@ usr, k, in_file, out_file = getParam(sys.argv)
 
 # usr \t message
 tweets = sc.textFile(in_file) \
-        .map(lambda line: (line.split("\t"))) \
+        .map(lambda line: (line.split("\t")))
 
 # usr document
 usr_tweets = tweets.filter(lambda x: x[0] == usr) \
         .reduceByKey(lambda x,y: x+y) \
         .flatMapValues(lambda x: x.split(" ")) \
-        .values() \
+        .values()
 
 words = tweets.flatMapValues(lambda x: (x.split(" ")))
 
+user_words = sc.broadcast(usr_tweets.distinct().collect())
+words = words.filter(lambda (x,y): y in user_words.value)
+
+# count of words in tweet Y
+all_words = words.map(lambda (x,y): (x,1)) \
+        .reduceByKey(lambda x,y: x+y)
+
+# count of words in tweet X
 user_words = sc.broadcast(usr_tweets.collect())
 words_distinct = words.distinct() \
-        .filter(lambda (x,y): y in user_words.value) \
-        .map(lambda (x,y): (x,1)) \
-        .reduceByKey(lambda x,y: x+y) \
-        .map(lambda (x,y): (y,x)) \
-        .sortByKey(ascending=False)
+        .map(lambda (x,y): (x,user_words.value.count(y))) \
+        .reduceByKey(lambda x,y: x+y)
 
-user_words = sc.broadcast(usr_tweets.distinct().collect())
-
-words = words.filter(lambda (x,y): y in user_words.value) \
-        .map(lambda (x,y): (x,1)) \
-        .reduceByKey(lambda x,y: x+y) \
-        .map(lambda (x,y): (y,x)) \
-        .sortByKey(ascending=False)
-
-top_10_words = words.zipWithIndex() \
-        .filter(lambda x: x[1] < k) \
-        .keys() \
-
-top_10_words_distinct = words_distinct.zipWithIndex() \
-        .filter(lambda x: x[1] < k) \
-        .keys() \
-# Sort  Alphabetically then Numerical 
-top_10_words = top_10_words.union(top_10_words_distinct) \
-        .map(lambda (x,y): (y,x)) \
+similarity = all_words.join(words_distinct) \
+        .map(lambda (x,y): (x,min(y))) \
         .sortByKey(ascending=True) \
         .map(lambda (x,y): (y,x)) \
-        .sortByKey(ascending=False)
-
-top_10_words.zipWithIndex() \
+        .sortByKey(ascending=False) \
+        .zipWithIndex() \
         .filter(lambda x: x[1] < k) \
         .keys() \
-        .map(lambda x: "%s\t%s" %(x[0],x[1])) \
+        .map(lambda x: "%s\t%s" %(x[1],x[0])) \
         .coalesce(1, shuffle = False) \
         .saveAsTextFile(out_file)
